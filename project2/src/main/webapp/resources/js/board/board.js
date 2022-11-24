a94e56f2ec45326d3df0840c0c9abe1550034ba9
skclsso25 <110643104+Rhosungchan@users.noreply.github.com>
2022-11-24 11:57:01 +0900

    20221223 수업

--- project2/src/main/webapp/resources/js/board/board.js
+++ project2/src/main/webapp/resources/js/board/board.js
@@ -103,3 +103,19 @@ deleteBtn.addEventListener("click", () => {
 
 
 });
+
+
+
+
+// 수정 버튼
+const updateBtn = document.getElementById("updateBtn");
+
+updateBtn.addEventListener("click", () => {
+
+    //  /board/{boardCode}/{boardNo}/update?cp=10
+
+    // 상세 조회 :  /board/{boardCode}/{boardNo}/update    ?cp=10
+    location.href = location.pathname + "/update" + location.search
+
+
+});
